87c3fa5e5dfcc50c6266791a5e5b6d637b2ac401
kh <kuanhang@live.com>
2023-02-27 08:37:43 +0000

    sadssa

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hello there ! I am simple Java appjjjjtuvtrycccrqqqqqqqc");
+        System.out.println("Hello there ! I am simple Java appjjjjtuvtryccqqqc");
     }
 
     public String getMessage() {
