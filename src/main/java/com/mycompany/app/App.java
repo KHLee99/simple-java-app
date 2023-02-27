dc986763b658b528ea0f7fe732d8d969bc19b180
kh <kuanhang@live.com>
2023-02-27 08:35:12 +0000

    sfsafas

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hello there ! I am simple Java appjjjjtuvtrycccrc");
+        System.out.println("Hello there ! I am simple Java appjjjjtuvtrycccrqqqqqqqc");
     }
 
     public String getMessage() {
