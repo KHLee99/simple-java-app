8e49c3e87bf7a835e8dc999756034d7c554ae127
kh <kuanhang@live.com>
2023-02-27 07:11:42 +0000

    change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hello there ! I am simple Java appjjjjjui,jknj");
+        System.out.println("Hello there ! I am simple Java appjjjjtuvtrycccrc");
     }
 
     public String getMessage() {
